c83304a46e2b64478668fad53a69a7971a818691
Filipe Mendes da Rocha Lopes <filipemendespi@gmail.com>
2017-11-30 00:26:40 -0300

    Configuring helps to flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def bootstrap_icon_for flash_type
+    {
+      success: 'check-circle',
+      error: 'times-circle',
+      alert: 'warning',
+      notice: 'info-circle'
+    }[flash_type.to_sym] || 'question-sign'
+  end
 end
