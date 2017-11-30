dfccd8f60cb8401c1ac14d13dd90bcd8806a4523
Filipe Mendes da Rocha Lopes <filipemendespi@gmail.com>
2017-11-30 00:26:07 -0300

    Configuring application_controller to allow access only by authenticated users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  rescue_from CanCan::AccessDenied do
+    flash[:error] = I18n.t('cancan.not_access')
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.js do
+        render :update do |page|
+          page.redirect_to root_url
+        end
+      end
+    end
+  end
 end
